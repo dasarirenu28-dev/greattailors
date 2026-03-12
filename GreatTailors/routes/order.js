98b552462a0696ffdd5239d86bbb2b2f8495d716
Renuka <dasarirenu28@gmail.com>
2026-03-12 19:04:29 +0530

    fix order model import

--- GreatTailors/routes/order.js
+++ GreatTailors/routes/order.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Order = require("./models/order");
+const Order = require("../models/order");
 const Payment = require("../module/payment");
 
 const router = express.Router();
