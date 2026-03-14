7e6ca1b36814b49cccff6f426c9cf169eddfc3fa
Renuka <dasarirenu28@gmail.com>
2026-03-14 19:07:07 +0530

    fix: update MongoDB connection string

--- GreatTailors/server.js
+++ GreatTailors/server.js
@@ -4,8 +4,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const helmet = require("helmet");
-require("dotenv").config();
-
+//require("dotenv").config();
+require("dotenv").config({ path: "./.env" });
 // Import Routes (CHECK SPELLING!)
 const customerRoutes = require("./routes/customer");
 const orderRoutes = require("./routes/order");
